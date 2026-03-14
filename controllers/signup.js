f74d5db4979d5164a92f98864cecb202ff8ae5de
spindling <jeremy.sean009@gmail.com>
2026-03-14 18:15:25 -0400

    Created signup.js and implemented page render

---
+++ controllers/signup.js
@@ -0,0 +1,17 @@
+const express = require('express');
+var router = express.Router()
+const UsersModel = require('../models/users.js')
+const bcrypt = require('bcrypt');
+
+// Displays the login page
+router.get("/", async function(req, res)
+{
+  // if we had an error during form submit, display it, clear it from session
+  req.TPL.login_error = req.session.login_error;
+  req.session.login_error = "";
+
+  // render the login page
+  res.render("signup", req.TPL);
+});
+
+module.exports = router;
