2f9126e8d7fe9a01fdd61149a9f451ae27d816f5
spindling <jeremy.sean009@gmail.com>
2026-03-14 20:36:08 -0400

    Implemented username/password length validation for signup and created red-text warning

--- controllers/signup.js
+++ controllers/signup.js
@@ -7,8 +7,8 @@ const bcrypt = require('bcrypt');
 router.get("/", async function(req, res)
 {
   // if we had an error during form submit, display it, clear it from session
-  req.TPL.login_error = req.session.login_error;
-  req.session.login_error = "";
+  req.TPL.signup_error = req.session.signup_error;
+  req.session.signup_error = "";
 
   // render the login page
   res.render("signup", req.TPL);
@@ -20,7 +20,8 @@ router.post("/attemptsignup", async function(req, res)
     //validation
     if (req.body.username.length < 6 || req.body.password.length < 6)
     {
-        console.log("Too short!");
+        req.session.signup_error = "Username/password cannot be less than 6 characters in length!"
+        res.redirect("/signup");
     }
 
 });
