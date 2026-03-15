b3a42b7b1962a18b18152cc3831809b7e336a02c
spindling <jeremy.sean009@gmail.com>
2026-03-14 22:26:35 -0400

    Fixed typo. Now renders, but does not protect editors page as desired

--- controllers/editors.js
+++ controllers/editors.js
@@ -4,7 +4,7 @@ var router = express.Router()
 // Display the editors page
 router.get("/", async function(req, res)
 {
-    res.render("/editors", req.TPL);
+    res.render("editors", req.TPL);
 
 });
 
