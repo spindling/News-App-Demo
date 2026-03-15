f02cd6e662b2db5a7abe25dfbe72465b7220e153
spindling <jeremy.sean009@gmail.com>
2026-03-14 22:04:06 -0400

    Implement editor access restriction check whenever editor page is rendered

--- controllers/editors.js
+++ controllers/editors.js
@@ -4,7 +4,16 @@ var router = express.Router()
 // Display the editors page
 router.get("/", async function(req, res)
 {
-  res.render("editors", req.TPL);
+
+  //Restrict access to editor's page
+  if (req.session.level == "editor") {
+    res.render("editors", req.TPL);
+
+  }
+  else
+  {
+    res.redirect("/home");
+  }
 });
 
 module.exports = router;
