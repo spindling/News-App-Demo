06831e6d15fd12fe65a88a732b712384634f79ae
spindling <jeremy.sean009@gmail.com>
2026-03-14 23:01:10 -0400

    Set Users value for editors.mustache when rendering editors page. Does not currently display table rows or values

--- controllers/editors.js
+++ controllers/editors.js
@@ -1,12 +1,15 @@
 const express = require('express');
 var router = express.Router()
+const UsersModel = require('../models/users.js')
+
 
 // Display the editors page
 router.get("/", async function(req, res)
-{
+{ 
+    const users = UsersModel.retrieveAllUsers();
+    req.TPL.Users = users;
     res.render("editors", req.TPL);
 
 });
-
 
 module.exports = router;
