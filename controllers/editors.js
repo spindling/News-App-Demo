38d97011df3c41e4266c15e9caeb4569a913c06c
spindling <jeremy.sean009@gmail.com>
2026-03-14 23:13:51 -0400

    Implement rendering of articles table when editors page is called

--- controllers/editors.js
+++ controllers/editors.js
@@ -1,14 +1,16 @@
 const express = require('express');
 var router = express.Router()
 const UsersModel = require('../models/users.js')
-
+const ArticlesModel = require('../models/articles.js')
 
 // Display the editors page
 router.get("/", async function(req, res)
 { 
     const users = await UsersModel.retrieveAllUsers();
-   
     req.TPL.Users = users;
+
+    const articles = await ArticlesModel.getAllArticles();
+    req.TPL.Articles = articles;
     res.render("editors", req.TPL);
 
 });
