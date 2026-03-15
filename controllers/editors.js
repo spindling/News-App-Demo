406535a190fbe35e045e7426ecdc4b498a89adff
spindling <jeremy.sean009@gmail.com>
2026-03-14 22:19:34 -0400

    Attempt to refactor editor route redirect to function as middleware. Currently not rendering /editors

--- controllers/editors.js
+++ controllers/editors.js
@@ -4,16 +4,23 @@ var router = express.Router()
 // Display the editors page
 router.get("/", async function(req, res)
 {
+    res.render("/editors", req.TPL);
 
-  //Restrict access to editor's page
-  if (req.session.level == "editor") {
-    res.render("editors", req.TPL);
+});
 
+function editorAccess(req,res,next)
+{
+    //Restrict access to editor's page
+  if (req.session.level == "editor")
+  {
+    next();
   }
   else
   {
     res.redirect("/home");
   }
-});
 
+}
+
+router.use(editorAccess);
 module.exports = router;
