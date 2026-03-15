92b2d8570423ce5ad14d0fe0dc8c224192b3a3f9
spindling <jeremy.sean009@gmail.com>
2026-03-14 20:42:05 -0400

    Set up sign-up confirmation message if username/password is valid

--- controllers/signup.js
+++ controllers/signup.js
@@ -4,14 +4,17 @@ const UsersModel = require('../models/users.js')
 const bcrypt = require('bcrypt');
 
 // Displays the login page
-router.get("/", async function(req, res)
-{
-  // if we had an error during form submit, display it, clear it from session
-  req.TPL.signup_error = req.session.signup_error;
-  req.session.signup_error = "";
+router.get("/", async function (req, res) {
+    // if we had an error during form submit, display it, clear it from session
+    req.TPL.signup_error = req.session.signup_error;
+    req.session.signup_error = "";
 
-  // render the login page
-  res.render("signup", req.TPL);
+    // render the signup page
+
+    req.TPL.signup_confirmation = req.session.signup_confirmation;
+    req.session.signup_confirmation = "";
+
+    res.render("signup", req.TPL);
 });
 
 
@@ -22,6 +25,12 @@ router.post("/attemptsignup", async function(req, res)
     {
         req.session.signup_error = "Username/password cannot be less than 6 characters in length!"
         res.redirect("/signup");
+    }
+    else
+    {
+        req.session.signup_confirmation = "User account created! <a href=\"/login\">Login</a> to access your new account.";
+        res.redirect("/signup");
+
     }
 
 });
